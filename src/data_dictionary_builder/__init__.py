c11c6d7d3eef552affab23056fbe6e7305102eef
GraFreak <isaiah.johnson@moniepoint.com>
2026-03-14 19:34:45 +0100

    Release: v0.1.2

--- src/data_dictionary_builder/__init__.py
+++ src/data_dictionary_builder/__init__.py
@@ -11,7 +11,7 @@ from .notifications.slack_notifier import SlackNotifier
 from .DDHelper import DDHelper
 from .timer import ExecutionTimer
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __all__ = [
     "MetadataExtractor",
     "DatabaseMetadata",
